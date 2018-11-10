efe03ea4c4516c41c157073c329aa3e59e28e223
Naoaki Iwakiri <naokiri@gmail.com>
2018-11-10 16:04:31 +0900

    will_process for a only IM

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate bitmask;
-#[macro_use]
-extern crate log;
+//#[macro_use]
+//extern crate log;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
@@ -10,13 +10,10 @@ extern crate toml;
 extern crate xkbcommon;
 
 use keyevent::KeyEvent;
-use keyevent::KeyEventSeq;
-use serde::Deserialize;
+
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::prelude::*;
-use std::mem;
-use xkbcommon::xkb;
+
+//use std::mem;
 
 mod keyevent;
 
@@ -39,36 +36,67 @@ enum Instruction {
 }
 
 /// Rough design prototype yet
+/// SKKの入力モード
+/// DDSKK 16.2 マニュアル 4.2 に依る
 pub(crate) enum InputMode {
+    // かなモード
     Hiragana,
+    // カナモード
     Katakana,
+    // JIS X 0201 カナ、 いわゆる半角カナ。 DDSKKでは独立したモード扱いではないので実装未定
     HankakuKatakana,
+    // 全英モード
     Zenkaku,
+    // アスキーモード
     Ascii,
 }
 
 /// Rough design prototype yet
-pub(crate) enum CompositionState {
+/// SKKの変換モード
+/// DDSKK 16.2 マニュアル 4.3 に依る
+pub(crate) enum CompositionMode {
+    // ■モード
     Direct,
+    // ▽モード
     PreComposition,
+    // ▼モード
     CompositionSelection,
-    // Sub-mode of PreComposition in ddskk
+    // SKK abbrev mode: Sub-mode of PreComposition
     Abbreviation,
-    // Sub-mode of CompositionSelection in ddskk
-    Register(Box<CompositionState>),
+    // Sub-mode of CompositionSelection
+    Register(Box<CompositionMode>),
 }
 
 /// Rough design prototype yet
 pub(crate) struct CskkContext {
     state_stack: Vec<CskkState>,
+    handler: AHandler,
 }
 
 struct CskkState {
     input_mode: InputMode,
-    composition_state: CompositionState,
+    composition_mode: CompositionMode,
     pre_composition: Option<String>,
     unconverted: String,
     converted: String,
+}
+
+struct AHandler {
+    process_list: HashMap<KeyEvent, String>,
+}
+
+impl AHandler {
+    pub fn new() -> AHandler {
+        let mut process_list = HashMap::new();
+        process_list.insert(KeyEvent::from_str("a").unwrap(),"あ".to_string());
+        AHandler {
+            process_list,
+        }
+    }
+
+    pub fn can_process(&self, key_event: &KeyEvent) -> bool {
+        self.process_list.contains_key(key_event)
+    }
 }
 
 impl CskkContext {
@@ -115,19 +143,35 @@ impl CskkContext {
 //        self.input_mode = new_mode;
 //    }
 
+    ///
+    /// Returns if that key event can be processed by current IM
+    ///
+    pub fn will_process(&self, key_event: &KeyEvent) -> bool {
+        let current_state = self.state_stack.last().unwrap();
+        let handler = self.get_handler(&current_state.input_mode, &current_state.composition_mode);
+        handler.can_process(key_event)
+    }
+
+    fn get_handler(&self, _input_mode: &InputMode, _composition_mode: &CompositionMode ) -> &AHandler {
+        return &self.handler;
+    }
+
     pub fn new(input_mode: InputMode,
-               composition_state: CompositionState,
+               composition_mode: CompositionMode,
                pre_composition: Option<String>) -> CskkContext {
+        let handler = AHandler::new();
+
         let mut initial_stack = Vec::new();
         initial_stack.push(CskkState {
             input_mode,
-            composition_state,
+            composition_mode,
             pre_composition,
             unconverted: "".to_string(), // TODO
             converted: "".to_string(), // TODO
         });
         CskkContext {
             state_stack: initial_stack,
+            handler,
         }
     }
 }
@@ -144,7 +188,7 @@ fn new_context() -> Box<CskkContext> {
     Box::new(
         CskkContext::new(
             InputMode::Hiragana,
-            CompositionState::Direct,
+            CompositionMode::Direct,
             None,
         ))
 //    mem::transmute(initial_context)
@@ -155,7 +199,25 @@ mod tests {
     use keyevent::KeyEvent;
     use super::*;
 
-    extern crate env_logger;
+
+    #[test]
+    fn will_process() {
+        let cskkcontext = CskkContext::new(
+            InputMode::Ascii,
+            CompositionMode::Direct,
+            None,
+        );
+        let a = KeyEvent::from_str("a").unwrap();
+        assert!(cskkcontext.will_process(&a));
+        let copy = KeyEvent::from_str("C-c").unwrap();
+        assert!(!cskkcontext.will_process(&copy));
+    }
 
 
+//    #[test]
+//    fn process_key_event() {
+//        skkcontext.process_key_event("a");
+//        let actual = skkcontext.poll_output();
+//        assert_eq!("あ", actual);
+//    }
 }
