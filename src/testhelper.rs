f6135054fc495ce1c0e0beeda9b45fea0844ca6a
Naoaki Iwakiri <naokiri@gmail.com>
2021-05-02 18:51:09 +0900

    Fix #80 consume period

---
+++ src/testhelper.rs
@@ -0,0 +1,9 @@
+pub fn init_test_logger() {
+    let _ = env_logger::builder()
+        // Include all events in tests
+        .filter_level(log::LevelFilter::max())
+        // Ensure events are captured by `cargo test`
+        .is_test(true)
+        // Ignore errors initializing the logger if tests race to configure it
+        .try_init();
+}
