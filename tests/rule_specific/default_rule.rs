c346db71563ffecb89c76d6ed9d0e1a393dc7dc7
Naoaki Iwakiri <naokiri@gmail.com>
2022-08-06 14:09:53 +0900

    Fix default rule

---
+++ tests/rule_specific/default_rule.rs
@@ -0,0 +1,21 @@
+use crate::utils::{
+    default_test_context, init_test_logger, test_context_with_dictionaries, transition_check,
+};
+use cskk::skk_context_reset_rs;
+use cskk::skk_modes::{CompositionMode, InputMode};
+
+#[test]
+fn z_slash() {
+    init_test_logger();
+    let mut context = default_test_context();
+    transition_check(
+        &mut context,
+        CompositionMode::Direct,
+        InputMode::Hiragana,
+        "z slash",
+        "",
+        "・",
+        InputMode::Hiragana,
+    );
+    skk_context_reset_rs(&mut context);
+}
---
+++ tests/rule_specific_tests.rs
@@ -0,0 +1,3 @@
+// 通常のテスト類もデフォルトのdefaultルールに依存してしまっているが、特にルールファイルのみをチェックするためのテスト群
+mod rule_specific;
+pub(crate) mod utils;
