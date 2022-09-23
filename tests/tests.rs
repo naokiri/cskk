1e8cb4db5de2d5c7164531a71ef39e70340730b7
Naoaki Iwakiri <naokiri@gmail.com>
2022-09-24 05:57:38 +0900

    Add tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1282,3 +1282,51 @@ fn backspace_to_abort() {
     );
     skk_context_reset_rs(&mut context);
 }
+
+#[test]
+fn prevent_no_text_okurigana() {
+    init_test_logger();
+    let mut context = default_test_context();
+    transition_check(
+        &mut context,
+        CompositionMode::Direct,
+        InputMode::Hiragana,
+        "Q S a",
+        "▽さ",
+        "",
+        InputMode::Hiragana,
+    );
+    skk_context_reset_rs(&mut context);
+}
+
+#[test]
+fn prevent_no_text_henkan() {
+    init_test_logger();
+    let mut context = default_test_context();
+    transition_check(
+        &mut context,
+        CompositionMode::Direct,
+        InputMode::Hiragana,
+        "Q space",
+        "▽",
+        "",
+        InputMode::Hiragana,
+    );
+    skk_context_reset_rs(&mut context);
+}
+
+#[test]
+fn prevent_no_kana_henkan() {
+    init_test_logger();
+    let mut context = default_test_context();
+    transition_check(
+        &mut context,
+        CompositionMode::Direct,
+        InputMode::Hiragana,
+        "Q k space",
+        "▽k",
+        "",
+        InputMode::Hiragana,
+    );
+    skk_context_reset_rs(&mut context);
+}
