e028ac9da59cd412cbf665314828da699b0a70b8
Naoaki Iwakiri <naokiri@gmail.com>
2025-02-24 14:57:37 +0900

    Fix Issue#261. Fix dictionary dedup wasn't respecting annotation

--- tests/dictionaries.rs
+++ tests/dictionaries.rs
@@ -1,18 +1,28 @@
-use std::sync::Arc;
-use cskk::cskkstate::{CompositionSelectionData, CskkStateInfo, DirectData};
+use crate::utils::{init_test_logger, test_context_with_dictionaries, transition_test};
+use cskk::cskkstate::{CompositionSelectionData, CskkStateInfo};
 use cskk::dictionary::CskkDictionary;
 use cskk::skk_context_reset_rs;
 use cskk::skk_modes::{CompositionMode, InputMode};
-use crate::utils::{default_test_context, init_test_logger, test_context_with_dictionaries, transition_test};
+use std::fs::copy;
+use std::sync::Arc;
 
 mod utils;
 
+///
+/// 重複した候補も別アノテーションならば別候補として扱う。
+/// 完全重複した場合は順序変更なく、最初に読み込まれた順番で扱う。
+///
 #[test]
-pub fn completion_mode_from_direct() {
+pub fn dictionary_order_should_be_preserved() {
     init_test_logger();
-    let dict1 =
-        CskkDictionary::new_static_dict("tests/data/dictionaries/order1.dat", "utf-8", false)
-            .unwrap();
+    // Overwrite to the expected default order
+    copy(
+        "tests/data/dictionaries/order1.dat.orig",
+        "tests/data/dictionaries/order1.dat",
+    )
+    .expect("TODO: Failed to copy and init the user dict.");
+    let dict1 = CskkDictionary::new_user_dict("tests/data/dictionaries/order1.dat", "utf-8", false)
+        .unwrap();
     let dict2 =
         CskkDictionary::new_static_dict("tests/data/dictionaries/order2.dat", "utf-8", false)
             .unwrap();
@@ -21,14 +31,14 @@ pub fn completion_mode_from_direct() {
         &mut context,
         CompositionMode::Direct,
         InputMode::Hiragana,
-        "T a t i space",
+        "C h o u space",
         CompositionMode::CompositionSelection,
         InputMode::Hiragana,
-        CskkStateInfo::CompositionSelection(CompositionSelectionData{
+        CskkStateInfo::CompositionSelection(CompositionSelectionData {
             confirmed: "".to_string(),
-            composited:"太刀".to_string(),
+            composited: "腸".to_string(),
             okuri: None,
-            annotation: None
+            annotation: None,
         }),
     );
     skk_context_reset_rs(&mut context);
@@ -36,14 +46,14 @@ pub fn completion_mode_from_direct() {
         &mut context,
         CompositionMode::Direct,
         InputMode::Hiragana,
-        "T a t i space space",
+        "C h o u space space",
         CompositionMode::CompositionSelection,
         InputMode::Hiragana,
-        CskkStateInfo::CompositionSelection(CompositionSelectionData{
+        CskkStateInfo::CompositionSelection(CompositionSelectionData {
             confirmed: "".to_string(),
-            composited:"大刀".to_string(),
+            composited: "超".to_string(),
             okuri: None,
-            annotation: None
+            annotation: None,
         }),
     );
     skk_context_reset_rs(&mut context);
@@ -51,14 +61,29 @@ pub fn completion_mode_from_direct() {
         &mut context,
         CompositionMode::Direct,
         InputMode::Hiragana,
-        "T a t i space space space",
+        "C h o u space space space",
         CompositionMode::CompositionSelection,
         InputMode::Hiragana,
-        CskkStateInfo::CompositionSelection(CompositionSelectionData{
+        CskkStateInfo::CompositionSelection(CompositionSelectionData {
             confirmed: "".to_string(),
-            composited:"達".to_string(),
+            composited: "帳".to_string(),
             okuri: None,
-            annotation: None
+            annotation: None,
+        }),
+    );
+    skk_context_reset_rs(&mut context);
+    transition_test(
+        &mut context,
+        CompositionMode::Direct,
+        InputMode::Hiragana,
+        "C h o u space space space space",
+        CompositionMode::CompositionSelection,
+        InputMode::Hiragana,
+        CskkStateInfo::CompositionSelection(CompositionSelectionData {
+            confirmed: "".to_string(),
+            composited: "長".to_string(),
+            okuri: None,
+            annotation: Some("人名".to_string()),
         }),
     );
 }
