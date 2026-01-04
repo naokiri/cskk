3e8f7712a1f8cb135bce31d24525034c586c0719
Naoaki Iwakiri <naokiri@gmail.com>
2026-01-04 15:06:29 +0900

    Fix the deb file didn't have so symlink

--- deploy_scripts/generate_deb_symlinks.sh
+++ deploy_scripts/generate_deb_symlinks.sh
@@ -5,5 +5,6 @@ version=`./deploy_scripts/version.sh`
 major_version=`./deploy_scripts/major_version.sh`
 
 (cd cskk/deb_assets &&
+ln -sf libcskk.so.$version libcskk.so &&
 ln -sf libcskk.so.$version libcskk.so.$major_version &&
 ln -sf libcskk.so.$major_version libcskk.so)
